50817c189aaa1e07df7d5e5c050d624dfc55c41a
Patryk Malinowski <pm3g19@soton.ac.uk>
2020-04-15 12:55:30 +0100

    Sending of data succeeded

--- main.c
+++ main.c
@@ -6,21 +6,39 @@
 #include <errno.h>
 
 
+
 int main(int argc, char** argv) {
-    int outSoc = socket(PF_INET, SOCK_DGRAM, IPPROTO_ICMP);
+    int outSoc = socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
     if (outSoc == -1) {
         printf("Socket Error.\n");
     }
 
+    struct in_addr dst;
+    if (inet_aton("216.58.210.227", &dst) == 0) {
+        printf("Invalid IP address.\n");
+    }
+
+    struct icmphdr header;
+    header.type = ICMP_ECHO;
+    header.un.echo.id = 1234;//arbitrary id
+
+    char buffer[2048];
+    const char* text = "hello world";
+
+    memcpy(buffer, &header, sizeof header);
+    memcpy(buffer + sizeof header, text, strlen(text));
     struct sockaddr_in server_desc;
-    server_desc.sin_addr.s_addr = inet_addr("92.222.139.190");
+    //memset(&server_desc, 0, sizeof server_desc);
+    server_desc.sin_addr = dst;
     server_desc.sin_family = AF_INET;
 
 
-    if(connect(outSoc, (struct sockaddr *)&server_desc, sizeof(server_desc)) < 0) {
-        printf("Connection error.");
-    }
-    if (send(outSoc, "hello", 5, 0) < 0) {
+
+    if (sendto(outSoc, buffer, sizeof buffer, 0, (struct sockaddr*)&server_desc, sizeof(server_desc)) < 0) {
         printf("Send failed. Error code: %d\n", errno);
+    } else {
+        printf("Send succeeded\n");
     }
+
+
 }
